cad391353db3c4bcca77e53270e9a646eb4e132b
TatianaSav <121678818+TatianaSav@users.noreply.github.com>
2023-06-12 15:48:23 +0400

    added functionality for adding images using URL to input

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ window.addEventListener("load", function () {
         } else {
             fabElement.style.display = "none";
         }
-    })
+    });
 });
 
 // When the user clicks on the button, scroll to the top of the document
@@ -24,3 +24,37 @@ function deletePhoto(id) {
     let elem = document.getElementById(id);
     elem.remove();
 }
+
+//Adding images by url in input
+function displayImage() {
+    const container = document.getElementById('picture-container');
+
+    // Get the URL input from user
+    const imgUrl = document.getElementById('AddPicture').value;
+
+    //Creating elements for support new pictures
+    let columnElement = document.createElement('div');
+    columnElement.classList.add('col-12', 'col-md-6', 'col-lg-3');
+    columnElement.id = String(new Date().getTime());
+
+    let pictureElement = document.createElement('div');
+    pictureElement.classList.add('picture');
+    pictureElement.style.backgroundImage = `url(${imgUrl})`;
+    columnElement.appendChild(pictureElement);
+
+    let buttonElement = document.createElement('button');
+    buttonElement.classList.add('btn', 'btn-dark', 'btn-custom', 'mt-2');
+    buttonElement.onclick = function () {
+        deletePhoto(columnElement.id);
+    };
+    buttonElement.innerText = "X";
+    pictureElement.appendChild(buttonElement);
+
+    //Put the new image at the top of the list
+    container.insertBefore(columnElement, container.firstChild);
+
+    //After executing the request, I remove the url from the input
+    document.getElementById('AddPicture').value = "";
+}
+
+
